d5e84753523273dced4c2d885fc8cbdab3aa7e60
Tuan Hai <tuanhai2712@gmail.com>
2020-07-13 00:23:25 +0700

    fix seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,32 +12,15 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $name = ['Tuấn Hải', 'Hương Trang', 'Thùy Linh', 'Tùng Lâm', 'Long'];
-        $address = ['Lạng Sơn', 'Hà Nội', 'Cao Bằng', 'Bắc Cạn', 'Hồ Chí Mình'];
-        $phoneNumber = ['0912312313', '012312879', '1230123102', '091283192', '0947271292'];
-        for ($i=1; $i < 150 ; $i++) {
-          $data = [
-              'name' => $name[array_rand($name, 1)],
-              'email' => 'test'.$i.'@gmail.com',
-              'address' => $address[array_rand($address, 1)],
-              'phone_number' => $phoneNumber[array_rand($phoneNumber, 1)],
-              'role' => 1,
-              'password' => bcrypt('12345678'),
-              'price_list' => '1',
-              'created_at' => new Datetime(),
-          ];
-          DB::table('users')->insert($data);
-        }
-
-        // $data = [
-        //   'name' => 'Nguyễn Tuấn Hải',
-        //   'email' => 'tuanhai2712@gmail.com',
-        //   'address' => 'Thôn Hoàng Tân - Xã Hoàng Đồng - TP.Lạng Sơn',
-        //   'phone_number' => '0947271292',
-        //   'role' => 999,
-        //   'password' => bcrypt('12345678'),
-        //   'created_at' => new Datetime(),
-        // ];
-        // DB::table('users')->insert($data);
+        $data = [
+          'name' => 'Nguyễn Tuấn Hải',
+          'email' => 'tuanhai2712@gmail.com',
+          'address' => 'Thôn Hoàng Tân - Xã Hoàng Đồng - TP.Lạng Sơn',
+          'phone_number' => '0947271292',
+          'role' => 999,
+          'password' => bcrypt('12345678'),
+          'created_at' => new Datetime(),
+        ];
+        DB::table('users')->insert($data);
     }
 }
